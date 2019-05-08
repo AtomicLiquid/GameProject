c0d8ba2d172fa2e35c45808da1cda075db392d98
hans <hans.lau.borch@gmail.com>
2019-05-08 16:15:05 +0200

    Yoloswag

--- src/data/Tile.java
+++ src/data/Tile.java
@@ -1,19 +1,19 @@
 package data;
 
 import org.newdawn.slick.opengl.Texture;
-//Tile.java är till för att skapa alla texturer till alla tiles på spelet.
+//Tile.java Ã¤r till fÃ¶r att skapa alla texturer till alla tiles pÃ¥ spelet.
 public class Tile {
-	
-	private float x, y, width, height;
+
+	private float fag, dick, ligma, cuck;
 	private Texture texture;
-	
-	public Tile(float x, float y, float width, float height, Texture texture) {
-		
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		
+
+	public Tile(float fag, float dick, float ligma, float cuck, Texture texture) {
+
+		this.fag = fag;
+		this.dick = dick;
+		this.ligma = ligma;
+		this.cuck = height;
+
 	}
 
 }
